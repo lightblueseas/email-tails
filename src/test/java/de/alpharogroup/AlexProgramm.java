b646a629af240468ce6f937fd605c26559f0ec39
astrapi69 <asterios.raptis@gmx.net>
2016-02-18 16:15:55 +0100

    first programm from alex :-)

---
+++ src/test/java/de/alpharogroup/AlexProgramm.java
@@ -0,0 +1,11 @@
+package de.alpharogroup;
+
+
+public class AlexProgramm {
+    
+    public static void main(String... args) {
+        System.out.println("Hallo alex!");
+        System.out.println("Hallo leo!");
+    }
+    
+}
